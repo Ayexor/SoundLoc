b971e4cbc0a1f061a47af5a19470e328a0c7e454
rseitz <rseitz@hsr.ch>
2016-12-29 01:35:54 +0100

    Fehler korrigiert

--- IP/SDM_Decimator_1.0/drivers/SDM_Decimator_v1_0/src/SDM_Decimator.c
+++ IP/SDM_Decimator_1.0/drivers/SDM_Decimator_v1_0/src/SDM_Decimator.c
@@ -9,6 +9,9 @@
  *		bit 1:0:	Order select (0 to 3)
  *					Order 0 disables filter
  *		bit 2:		Enable interrupt on new value
+ *		bit 3:		Enable IIR DC blocker
+ *		bit 7:4:	IIR pole: by 1-2**unsigned(REG0(7:4))
+ *		bit 11:8:	CIC post divide by 2**unsigned(REG0(11:8)) 
  */
 void SDM_DECIM_setStatus(int BaseAddress, int Status){
 	SDM_DECIMATOR_mWriteReg(BaseAddress, SDM_DECIMATOR_S_AXI_REG0_STATUS_OFFSET, Status);
